7c4a2f7f0c7c23af1d245b73bd05fcf0c7ecd014
Susmita Shiwakoti <susmita.shiwakoti71@gmail.com>
2022-12-06 17:30:08 +0545

    SecondProject

---
+++ accounts/queryDemo.py
@@ -0,0 +1,7 @@
+# # Return all customer from customer table
+# customers = Customer.objects.all()
+
+# # return first customer in table
+# firstCustomer = Customer.objectd.first()
+
+# return signal
